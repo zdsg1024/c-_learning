28cac8b26c61688da76f3dc67c9f3ff0841e5408
zdsg1024 <923686423@qq.com>
2022-05-01 19:20:04 +0800

    basic grammar learning

---
+++ part_one_base/3-Exegesis.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 单行注释
+    /*
+    多行注释
+    多行注释
+    */
+    return 0;
+}
---
+++ part_one_base/4-variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 变量的定义
+    // 语法：数据类型 变量名称 = 初始值;
+
+    int a = 10;
+    cout << "a=" << a << endl;
+    return 0;
+}
